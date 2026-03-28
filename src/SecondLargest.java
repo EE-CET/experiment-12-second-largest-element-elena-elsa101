a9a1e1fd5a389192eb8ecbbe78b700129fd7d85b
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-28 22:59:04 +0530

    Update SecondLargest.java

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,7 +1,7 @@
     
 import java.util.*;
 public class SecondLargest {
- 
+
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
@@ -19,6 +19,5 @@ second=arr[i];
 }
 System.out.print(second);
 }
-
-
 }
+
